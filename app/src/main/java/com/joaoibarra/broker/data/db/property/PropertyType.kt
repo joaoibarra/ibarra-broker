16a257fee1825e74bb5752d7fcea41f85e6e81d9
Joao Ibarra <joao@anyplay.audio>
2021-03-14 16:17:32 +0100

    Fixes warnings

--- app/src/main/java/com/joaoibarra/broker/data/db/property/PropertyType.kt
+++ app/src/main/java/com/joaoibarra/broker/data/db/property/PropertyType.kt
@@ -1,6 +1,6 @@
 package com.joaoibarra.broker.data.db.property
 
-enum class PropertyType(type: String) {
+enum class PropertyType(val type: String) {
     HighlightedProperty("HighlightedProperty"),
     Property("Property"),
     Area("Area")
